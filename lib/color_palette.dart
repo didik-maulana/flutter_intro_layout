e2d4086cc993e572d28ba6903293ac2ddbdbd86f
Didik Maulana Ardiansyah <didikmaulana49@gmail.com>
2019-05-31 20:27:15 +0700

    add constans color and model

---
+++ lib/color_palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const dotColor = Color(0xffe8e8e8);
+  static const dotActiveColor = Color(0xffff3800);
+  static const titleColor = Color(0xff1c1a1a);
+  static const descriptionColor = Color(0xff707070);
+}
---
+++ lib/model/intro.dart
@@ -0,0 +1,7 @@
+class Intro {
+  String image;
+  String title;
+  String description;
+  
+  Intro({this.image, this.title, this.description});
+}
